e6de4c7e15fdf0a8596be6fddac8fa8dcca6380f
Liudmyla007 <ldatsenko3@gmail.com>
2023-03-28 19:24:25 +0300

    done

---
+++ src/components/StatusFilter/StatusFilter.js
@@ -0,0 +1,37 @@
+// Імпортуємо хук
+import { useSelector, useDispatch } from "react-redux";
+import { Button } from "components/Button/Button";
+// Імпортуємо генератор екшену
+import { setStatusFilter } from "../../redux/actions";
+// Імпортуємо об'єкт значень фільтра
+import { statusFilters } from "../../redux/constants";
+export const StatusFilter = () => {
+  // Отримуємо посилання на функцію відправки екшенів
+  const dispatch = useDispatch();
+  const filter = useSelector(state => state.statusFilter);
+  // Викликаємо генератор екшену та передаємо значення фільтра
+  // Відправляємо результат - екшен зміни фільтра
+  const handleFilterChange = filter => dispatch(setStatusFilter(filter));
+  return (
+    <div>
+      <Button
+        selected={filter === statusFilters.all}
+        onClick={() => handleFilterChange(statusFilters.all)}
+      >
+        All
+      </Button>
+      <Button
+        selected={filter === statusFilters.active}
+        onClick={() => handleFilterChange(statusFilters.active)}
+      >
+        Active
+      </Button>
+      <Button
+        selected={filter === statusFilters.completed}
+        onClick={() => handleFilterChange(statusFilters.completed)}
+      >
+        Completed
+      </Button>
+    </div>
+  );
+};
